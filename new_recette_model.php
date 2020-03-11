afd3ad81bad838c8780d744f19745e49f837f5a2
Exaci <59509642+Exaci@users.noreply.github.com>
2020-03-11 16:05:09 +0100

    Add files via upload

--- new_recette_model.php
+++ new_recette_model.php
@@ -8,7 +8,9 @@ include("../view/index_view.php");
 
 if ($ID==0) erreur(ERR_IS_NOT_CO);
 
-if(empty($_POST['name'])){ 
+if(empty($_POST['name'])){
+    $query= $base->prepare('DELETE FROM tempo');
+    $query->execute();
     $is=1;
     if(!isset($_GET['x'])){
         $query=$base->prepare('SELECT nomingredient FROM ingredients WHERE type="Legumes"');
@@ -59,5 +61,30 @@ else {
     'ingredients' => $ingredients,
     'etape'=> $etape
     ));
+
+    $queryrecette= $base->prepare('SELECT id_recette FROM asso');
+    $queryrecette->execute;
+    while ($data1=$queryrecette->fetch()){
+
+    }
+    $id_recette = $data1['id_recette']+1;
+    
+
+    $query = $base->prepare('SELECT idingredient,quantite,unite FROM tempo WHERE 1');
+    $query->execute();
+    $queryinsert = $base->prepare('INSERT INTO asso(id_ingredient,id_recette,quantite,type_quantite)VALUES(:id_ingredient, :id_recette, :quantite, :type_quantite)');
+    while($data=$query->fetch()){
+        $id_ingredient=$data['idingredient'];
+        $quantite = $data['quantite'];
+        $type_quantite = $data['unite'];
+        $queryinsert-execute(array(
+            'id_ingredient' => $id_ingredient,
+            'id_recette' => $id_recette,
+            'quantite' => $quantite,
+            'type_quantite' => $type_quantite
+        ));
+    }
+
+
     $is=2;
     }
